3a0c5d9c7fd2a998cf21de8795a8333e625401da
julian-b24 <julian.camilob24@gmail.com>
2023-09-30 20:56:08 -0500

    Movies and Character's details pages added to routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,15 +5,21 @@ import MoviesGrid from "../pages/MoviesGrid";
 import CharactersGrid from "../pages/CharactersGrid";
 import PotionsGrid from "../pages/PotionsGrid";
 import PotionDetails from "../pages/PotionDetails";
+import CharacterDetails from "../pages/CharacterDetails";
+import MovieDetails from "../pages/MovieDetails";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomeView />} />
-      <Route path="/movies" element={<MoviesGrid />} />
-      <Route path="/characters" element={<CharactersGrid />} />
+      <Route path="/movies" element={<MoviesGrid />}>
+        <Route path=":id" element={<MovieDetails />} />
+      </Route>
+      <Route path="/characters" element={<CharactersGrid />}>
+        <Route path=":id" element={<CharacterDetails />} />
+      </Route>
       <Route path="/potions" element={<PotionsGrid />}>
-        <Route path=":id" element={<PotionDetails/>} />
+        <Route path=":id" element={<PotionDetails />} />
       </Route>
     </Routes>
   </BrowserRouter>
