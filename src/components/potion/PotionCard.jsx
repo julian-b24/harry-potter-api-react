9fc417866388aa45a270a07714a4496c385fffe9
Juan Angulo <juanferas25@hotmail.com>
2023-10-01 12:40:57 -0500

    Character img update working

--- src/components/potion/PotionCard.jsx
+++ src/components/potion/PotionCard.jsx
@@ -1,55 +1,54 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-const DEFAULT_IMAGE = "";
+const DEFAULT_IMAGE =
+  "https://ar.happyvalentinesday2020.online/pics/webstockreview.net/images/cauldron-clipart-harry-potter-cauldron-8.jpg";
 function PotionCard({ potion }) {
-    
-    const navigate = useNavigate();
-    const [potionImage, setPotionImage] = useState(potion.attributes.image)
+  const navigate = useNavigate();
+  const [potionImage, setPotionImage] = useState(potion.attributes.image);
 
-    useEffect(() => { setDefaultImage()}, [])
-
-    const setDefaultImage = () => {
-        if(potionImage === null){
-            setPotionImage(DEFAULT_IMAGE)
-        }
-    }
-
-    return (
-        <Card sx={{ maxWidth: 345}}>
-            <CardMedia
-                component="img"
-                sx={{ height: 350 }}
-                height="140"
-                alt="potion"
-                image={potionImage}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {potion.attributes.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {potion.attributes.effect}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small" onClick={() => {navigate(`/potions/${potion.id}`)}} >Details and info.</Button>
-            </CardActions>
-        </Card>
-    )
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        sx={{ height: 350 }}
+        height="140"
+        alt="potion"
+        image={potionImage === null ? DEFAULT_IMAGE : potionImage}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {potion.attributes.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {potion.attributes.effect}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          onClick={() => {
+            navigate(`/potions/${potion.id}`);
+          }}
+        >
+          Details and info.
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
 
 PotionCard.propTypes = {
-    potion: PropTypes.object
-}
+  potion: PropTypes.object,
+};
 
-export default PotionCard
+export default PotionCard;
