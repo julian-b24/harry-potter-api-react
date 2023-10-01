733ec95147f73771c65f143e71b15f519b71dc99
julian-b24 <julian.camilob24@gmail.com>
2023-10-01 17:56:33 -0500

    Styling of movie's details

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -2,6 +2,7 @@ import axios from "../../config/axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import Grid from "@mui/material/Unstable_Grid2";
 import AspectRatio from "@mui/joy/AspectRatio";
 import Box from "@mui/joy/Box";
 import Typography from "@mui/joy/Typography";
@@ -15,6 +16,7 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
 
+import { deepPurple } from "@mui/material/colors";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import LinkIcon from "@mui/icons-material/Link";
@@ -43,171 +45,203 @@ function MovieDetails() {
   return (
     <>
       {movie && (
-        <Box sx={{ width: "100%", borderRadius: "sm", p: 1 }}>
-          {/*Movie poster*/}
-          <AspectRatio objectFit="contain">
-            <img src={movie.attributes.poster} alt={movie.attributes.slug} />
-          </AspectRatio>
+        <Box sx={{
+          width: "100%",
+          borderRadius: "sm",
+          p: 4,
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}>
 
-          {/*Movie title*/}
-          <Box>
-            <Typography level="h3">{movie.attributes.title}</Typography>
-          </Box>
-
-          {/*Movie Summary*/}
-          <Box>
-            <Typography level="title-lg">Summary</Typography>
-            <Typography level="body-md">{movie.attributes.summary}</Typography>
-          </Box>
-
-          {/*Directors, Screenwriters, producers, cinematographers, editors and music acordeons*/}
-          <AccordionGroup sx={{ maxWidth: 400 }}>
-            <Accordion disabled={movie.attributes.driectors === null}>
-              <AccordionSummary>Directors</AccordionSummary>
-              <AccordionDetails>{movie.attributes.driectors}</AccordionDetails>
-            </Accordion>
-
-            <Accordion disabled={movie.attributes.screenwiters === null}>
-              <AccordionSummary>Screenwiters</AccordionSummary>
-              <AccordionDetails>
-                {movie.attributes.screenwiters}
-              </AccordionDetails>
-            </Accordion>
-
-            <Accordion disabled={movie.attributes.producers === null}>
-              <AccordionSummary>Producers</AccordionSummary>
-              <AccordionDetails>{movie.attributes.producers}</AccordionDetails>
-            </Accordion>
-
-            <Accordion disabled={movie.attributes.cinematographers === null}>
-              <AccordionSummary>Cinematographers</AccordionSummary>
-              <AccordionDetails>
-                {movie.attributes.cinematographers}
-              </AccordionDetails>
-            </Accordion>
-
-            <Accordion disabled={movie.attributes.editors === null}>
-              <AccordionSummary>Editors</AccordionSummary>
-              <AccordionDetails>{movie.attributes.editors}</AccordionDetails>
-            </Accordion>
-
-            <Accordion disabled={movie.attributes.music_composers === null}>
-              <AccordionSummary>Music</AccordionSummary>
-              <AccordionDetails>
-                {movie.attributes.music_composers}
-              </AccordionDetails>
-            </Accordion>
-          </AccordionGroup>
-
-          {/*Extra info: Release date, running time, budget, box office, rating, trailer link and wiki link*/}
-          <List
-            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            sx={{ display: "flexbox", justifyContent: "space-evenly" }}
           >
-            {/*Release date*/}
-            {movie.attributes.release_date && (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <DateRangeIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Release date"
-                  secondary={movie.attributes.release_date}
-                />
-              </ListItem>
-            )}
+            <Grid item xs={6} sx={{ display: "flex" }}>
+              {/*Movie poster*/}
+              <AspectRatio objectFit="contain">
+                <img src={movie.attributes.poster} alt={movie.attributes.slug} />
+              </AspectRatio>
+            </Grid>
 
-            {/*Running Time*/}
-            {movie.attributes.running_time && (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <AccessTimeIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Running Time"
-                  secondary={movie.attributes.running_time}
-                />
-              </ListItem>
-            )}
+            <Grid item xs={6}>
+              <div>
+                {/*Movie title*/}
+                <Box>
+                  <Typography level="h3">{movie.attributes.title}</Typography>
+                </Box>
 
-            {/*Budget*/}
-            {movie.attributes.budget && (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <AttachMoneyIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Budget"
-                  secondary={movie.attributes.budget}
-                />
-              </ListItem>
-            )}
+                {/*Movie Summary*/}
+                <Box>
+                  <Typography level="title-lg">Summary</Typography>
+                  <Typography level="body-md">{movie.attributes.summary}</Typography>
+                </Box>
+              </div>
 
-            {/*Box office*/}
-            {movie.attributes.box_office && (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <CurrencyExchangeIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Box office"
-                  secondary={movie.attributes.box_office}
-                />
-              </ListItem>
-            )}
 
-            {/*Rating*/}
-            {movie.attributes.rating && (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <ExplicitIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Rating"
-                  secondary={movie.attributes.rating}
-                />
-              </ListItem>
-            )}
+              {/*Directors, Screenwriters, producers, cinematographers, editors and music acordeons*/}
+              <AccordionGroup sx={{ maxWidth: "100%", fontSize: "1.2em" }}>
+                <Accordion size="lg" disabled={movie.attributes.directors === null}>
+                  <AccordionSummary>Directors</AccordionSummary>
+                  <AccordionDetails>{movie.attributes.directors}</AccordionDetails>
+                </Accordion>
 
-            {/*Trailer*/}
-            {movie.attributes.trailer && (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <YouTubeIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Trailer"
-                  secondary={movie.attributes.trailer}
-                />
-              </ListItem>
-            )}
+                <Accordion size="lg" disabled={movie.attributes.screenwriters === null}>
+                  <AccordionSummary>Screenwiters</AccordionSummary>
+                  <AccordionDetails>
+                    {movie.attributes.screenwriters}
+                  </AccordionDetails>
+                </Accordion>
 
-            {/*Wiki*/}
-            {movie.attributes.wiki && (
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>
-                    <LinkIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Wiki"
-                  secondary={movie.attributes.wiki}
-                />
-              </ListItem>
-            )}
-          </List>
+                <Accordion size="lg" disabled={movie.attributes.producers === null}>
+                  <AccordionSummary>Producers</AccordionSummary>
+                  <AccordionDetails>{movie.attributes.producers}</AccordionDetails>
+                </Accordion>
+
+                <Accordion size="lg" disabled={movie.attributes.cinematographers === null}>
+                  <AccordionSummary>Cinematographers</AccordionSummary>
+                  <AccordionDetails>
+                    {movie.attributes.cinematographers}
+                  </AccordionDetails>
+                </Accordion>
+
+                <Accordion size="lg" disabled={movie.attributes.editors === null}>
+                  <AccordionSummary>Editors</AccordionSummary>
+                  <AccordionDetails>{movie.attributes.editors}</AccordionDetails>
+                </Accordion>
+
+                <Accordion size="lg" disabled={movie.attributes.music_composers === null}>
+                  <AccordionSummary>Music</AccordionSummary>
+                  <AccordionDetails>
+                    {movie.attributes.music_composers}
+                  </AccordionDetails>
+                </Accordion>
+              </AccordionGroup>
+
+            </Grid>
+
+            <Grid item sx={{display: 'flex', justifyContent: 'flex-start'}}>
+              {/*Extra info: Release date, running time, budget, box office, rating, trailer link and wiki link*/}
+              <List
+                sx={{
+                  width: "100%",
+                  maxWidth: 360,
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "nowrap",
+                }}
+              >
+                {/*Release date*/}
+                {movie.attributes.release_date && (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar sx={{ bgcolor: deepPurple[400] }} >
+                        <DateRangeIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Release date"
+                      secondary={movie.attributes.release_date}
+                    />
+                  </ListItem>
+                )}
+
+                {/*Running Time*/}
+                {movie.attributes.running_time && (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar sx={{ bgcolor: deepPurple[400] }} >
+                        <AccessTimeIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Running Time"
+                      secondary={movie.attributes.running_time}
+                    />
+                  </ListItem>
+                )}
+
+                {/*Budget*/}
+                {movie.attributes.budget && (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar sx={{ bgcolor: deepPurple[400] }} >
+                        <AttachMoneyIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Budget"
+                      secondary={movie.attributes.budget}
+                    />
+                  </ListItem>
+                )}
+
+                {/*Box office*/}
+                {movie.attributes.box_office && (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar sx={{ bgcolor: deepPurple[400] }} >
+                        <CurrencyExchangeIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Box office"
+                      secondary={movie.attributes.box_office}
+                    />
+                  </ListItem>
+                )}
+
+                {/*Rating*/}
+                {movie.attributes.rating && (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar sx={{ bgcolor: deepPurple[400] }} >
+                        <ExplicitIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Rating"
+                      secondary={movie.attributes.rating}
+                    />
+                  </ListItem>
+                )}
+
+                {/*Trailer*/}
+                {movie.attributes.trailer && (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar sx={{ bgcolor: deepPurple[400] }} >
+                        <YouTubeIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Trailer"
+                      secondary={movie.attributes.trailer}
+                    />
+                  </ListItem>
+                )}
+
+                {/*Wiki*/}
+                {movie.attributes.wiki && (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar sx={{ bgcolor: deepPurple[400] }} >
+                        <LinkIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Wiki"
+                      secondary={movie.attributes.wiki}
+                    />
+                  </ListItem>
+                )}
+              </List>
+            </Grid>
+
+          </Grid>
         </Box>
       )}
     </>
